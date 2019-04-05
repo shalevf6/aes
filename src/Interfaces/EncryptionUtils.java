8b8f7c602cf0c1b1809db9ab4b0f174d64ed555d
shalevf6 <38422152+shalevf6@users.noreply.github.com>
2019-04-05 21:41:31 +0300

    Finished Encrypt. Added necessary static functions to EncryptionUtils

--- src/Interfaces/EncryptionUtils.java
+++ src/Interfaces/EncryptionUtils.java
@@ -7,7 +7,25 @@ public class EncryptionUtils {
      * @param block - a given block
      */
     public static void shiftRows(byte[][] block) {
+        shiftLeftInRow(block[1]);
+        shiftLeftInRow(block[2]);
+        shiftLeftInRow(block[2]);
+        shiftLeftInRow(block[3]);
+        shiftLeftInRow(block[3]);
+        shiftLeftInRow(block[3]);
+    }
 
+    /**
+     * Performs one shift to the left in a given array
+     * @param blockRow - a given array
+     */
+    private static void shiftLeftInRow(byte[] blockRow) {
+        byte temp = blockRow[0];
+        for(int i = 0; i < 3; i++)
+        {
+            blockRow[i] = blockRow[i + 1];
+        }
+        blockRow[3] = temp;
     }
 
     /**
@@ -15,7 +33,25 @@ public class EncryptionUtils {
      * @param block - a given block
      */
     public static void unShiftRows(byte[][] block) {
+        shiftRightInRow(block[1]);
+        shiftRightInRow(block[2]);
+        shiftRightInRow(block[2]);
+        shiftRightInRow(block[3]);
+        shiftRightInRow(block[3]);
+        shiftRightInRow(block[3]);
+    }
 
+    /**
+     * Performs one shift to the right in a given array
+     * @param blockRow - a given array
+     */
+    private static void shiftRightInRow(byte[] blockRow) {
+        byte temp = blockRow[3];
+        for(int i = 3; i > 0; i--)
+        {
+            blockRow[i] = blockRow[i - 1];
+        }
+        blockRow[0] = temp;
     }
 
     /**
@@ -24,7 +60,13 @@ public class EncryptionUtils {
      * @param k - a given encryption key
      */
     public static void addRoundKey(byte[][] currentBlock, byte[] k) {
-
+        int kCounter = 0;
+        for (int i = 0; i < currentBlock.length; i++) {
+            for (int j = 0; j < currentBlock[0].length; j++) {
+                currentBlock[j][i] = (byte)(currentBlock[j][i] ^ k[kCounter]);
+                kCounter++;
+            }
+        }
     }
 
     /**
@@ -34,7 +76,13 @@ public class EncryptionUtils {
      * @param blockCount - the given block's number
      */
     public static void addBlockToOutput(byte[] output, byte[][] currentBlock, int blockCount) {
-
+        int outputCounter = blockCount * 16;
+        for (int i = 0; i < currentBlock.length; i++) {
+            for (int j = 0; j < currentBlock[0].length; j++) {
+                output[outputCounter] = currentBlock[j][i];
+                outputCounter++;
+            }
+        }
     }
 
     public static void generateKeys() {
